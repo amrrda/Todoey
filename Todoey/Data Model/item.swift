fafb2b4b2a2662cb8fa1e8d7671f52ecae2ce351
Amr Reda <Amr@amrs-macbook-pro.local>
2018-10-18 14:11:06 +0200

    Convert to MVC designing pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Amr Reda on 10/18/18.
+//  Copyright © 2018 Amr Reda. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
